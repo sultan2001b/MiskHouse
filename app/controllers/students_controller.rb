73ae6699f0460339f969960cbd4d14769c4004ca
sultan2001b <sultan2001b@gmail.com>
2018-11-28 19:24:02 +0300

    fixing on seed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,23 @@
 class StudentsController < ApplicationController
   def new
+    @student = Student.new
   end
 
   def edit
+    @student = Stude
   end
 
   def show
   end
 
   def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to @student
+    else
+      render "new"
+    end
   end
 
   def update
@@ -16,4 +25,11 @@ class StudentsController < ApplicationController
 
   def index
   end
+
+  private
+  def student_params
+    params.require(:student).permit(:f_name, :l_name, :gender, :email, :mobile, :linkedin, :github_user, :twitter, :img, :bio)
+  end
+
 end
+
