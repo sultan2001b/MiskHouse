ea2ebfc1f91ef769fc85109b723b9cee959aa030
wh0-1-am <o.t5@hotmail.com>
2018-11-29 11:48:36 +0300

    add edit and new of Controller course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,8 +6,33 @@ class CoursesController < ApplicationController
   end
 
   def edit
+    @course = Course.find_by(id: params[:id])
+  end
+
+  def update
+    course = Course.find_by(id: params[:id])
+    course.update(course_params)
+    redirect_to courses_path
   end
 
   def new
+    @course = Course.new
+  end
+
+  def create
+    course = Course.create(course_params)
+    redirect_to courses_path
+  end
+
+  def destroy
+    course = Course.find_by(id: params[:id])
+    course.destroy
+    redirect_to courses_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :img, :short_description, :long_description)
   end
 end
