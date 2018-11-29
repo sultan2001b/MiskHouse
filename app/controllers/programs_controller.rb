1716640cd464beafb1bf33f1891abdf0307d846b
sultan2001b <sultan2001b@gmail.com>
2018-11-29 16:53:41 +0300

    add courses with program

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -5,6 +5,7 @@ class ProgramsController < ApplicationController
 
   def show
     @program = Program.find_by(id: params[:id])
+    
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :programs
   resources :courses
   resources :projects
- 
 end
