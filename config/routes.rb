4440cec05a0b38165de5c0df1b405033c8008561
wh0-1-am <o.t5@hotmail.com>
2018-11-29 10:26:05 +0300

    add Controller and view of Courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'students/new'
-  get 'students/edit'
-  get 'students/show'
-  get 'students/create'
-  get 'students/update'
-  get 'students/index'
+  resources :courses
+  get "students/new"
+  get "students/edit"
+  get "students/show"
+  get "students/create"
+  get "students/update"
+  get "students/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
