033974eef28697c0f893db3a2bb2b8a9bcec0342
sultan2001b <sultan2001b@gmail.com>
2018-12-03 15:15:48 +0300

    create student linked with user

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,9 @@
 class StudentsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def new
     @student = Student.new
-    
+    # @student.user_id = current_user.id
   end
 
   def edit
@@ -15,15 +16,30 @@ class StudentsController < ApplicationController
   end
 
   def create
-    course_id = session[:program_id]
-    course = Course.find_by(id: course_id)
-    @student = course.students.create(student_params)
+    student = Student.new
+student.f_name =student_params["f_name"];
+student.l_name =student_params["l_name"];
+student.gender =student_params["gender"];
+student.email =student_params["email"];
+student.mobile =student_params["mobile"];
+student.linkedin =student_params["linkedin"];
+student.github_user =student_params["github_user"];
+student.twitter =student_params["twitter"];
+student.img =student_params["img"];
+student.bio =student_params["bio"];
+student.user_id = current_user.id;
+
+ student.save;
+    # course_id = session[:program_id]
+    # course = Course.find_by(id: course_id)
+    # @student = course.students.create(student_params)
     # if @student.save
     #   redirect_to @student
     # else
     #   render "new"
     # end
-    redirect_to course_path(course)
+    # redirect_to root_path
+    redirect_to root_path
   end
 
   def update
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MiskHouse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.paths['app/views'] << "app/views/devise"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
