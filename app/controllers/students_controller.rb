845e95d30a29e7f9fe89b740443dd7e04e42aed2
sultan2001b <sultan2001b@gmail.com>
2018-12-03 13:44:15 +0300

    add relationship user student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,7 @@ class StudentsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def new
     @student = Student.new
+    
   end
 
   def edit
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,5 @@ class Student < ApplicationRecord
   has_and_belongs_to_many :projects
   has_and_belongs_to_many :tags
   # has_many :programs, through: :courses
+  belongs_to :user
 end
